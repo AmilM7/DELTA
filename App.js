dc8aceaf50417bcb7d067992c5d4db6915f8276c
IrfanParic <irfan.paric@stu.ssst.edu.ba>
2023-06-02 21:18:22 +0200

    Fixed: drawer styling

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import Dashboard from './screens/home/Dashboard'
 import Category from "./screens/category/Category";
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
-import { Appbar, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import {Appbar, IconButton, Provider as PaperProvider} from 'react-native-paper';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import colors from "./styles/colors";
 
@@ -32,44 +32,87 @@ export default function App() {
                         drawerActiveBackgroundColor: colors.Primary,
                         drawerActiveTintColor: colors.LightText,
                         drawerInactiveTintColor: colors.Primary,
-                        drawerType: 'front',
                     }}>
-                        <Drawer.Screen name="Home" component={Dashboard}/>
+                        <Drawer.Screen
+                            name="Home"
+                            component={Dashboard}
+                            options={{
+                                drawerIcon: ({ focused, size }) => (
+                                    <IconButton containerColor={colors.Primary} iconColor={colors.LightText} icon="home" />
+                                )
+                            }}
+                        />
                         <Drawer.Screen
                             name="Business"
                             component={Category}
                             initialParams={{category: "Business"}}
+                            options={{
+                                drawerIcon: ({ focused, size }) => (
+                                    <IconButton containerColor={colors.Primary} iconColor={colors.LightText} icon="briefcase" />
+                                )
+                            }}
                         />
                         <Drawer.Screen
                             name="Sport"
                             component={Category}
                             initialParams={{category: "Sport"}}
+                            options={{
+                                drawerIcon: ({ focused, size }) => (
+                                    <IconButton containerColor={colors.Primary} iconColor={colors.LightText} icon="volleyball" />
+                                )
+                            }}
                         />
                         <Drawer.Screen
                             name="Education"
                             component={Category}
                             initialParams={{category: "Education"}}
+                            options={{
+                                drawerIcon: ({ focused, size }) => (
+                                    <IconButton containerColor={colors.Primary} iconColor={colors.LightText} icon="school" />
+                                )
+                            }}
                         />
 
                         <Drawer.Screen
                             name="Travel"
                             component={Category}
                             initialParams={{category: "Travel"}}
+                            options={{
+                                drawerIcon: ({ focused, size }) => (
+                                    <IconButton containerColor={colors.Primary} iconColor={colors.LightText} icon="airplane" />
+                                )
+                            }}
                         />
                         <Drawer.Screen
                             name="Politics"
                             component={Category}
                             initialParams={{category: "Politics"}}
+                            options={{
+                                drawerIcon: ({ focused, size }) => (
+                                    <IconButton containerColor={colors.Primary} iconColor={colors.LightText} icon="tie" />
+                                )
+                            }}
                         />
                         <Drawer.Screen
                             name="Health"
                             component={Category}
                             initialParams={{category: "Health"}}
+                            options={{
+                                drawerIcon: ({ focused, size }) => (
+                                    <IconButton containerColor={colors.Primary} iconColor={colors.LightText} icon="hospital" />
+                                )
+                            }}
                         />
                         <Drawer.Screen
                             name="Entertainment"
                             component={Category}
                             initialParams={{category: "Entertainment"}}
+                            options={{
+                            drawerIcon: ({ focused, size }) => (
+                                <IconButton containerColor={colors.Primary} iconColor={colors.LightText}  icon="popcorn" />
+                            ),
+                        }}
+
                         />
                     </Drawer.Navigator>
                 </NavigationContainer>
@@ -102,4 +145,7 @@ const styles = StyleSheet.create({
     drawerStyle: {
         backgroundColor: colors.LightText,
     },
+    icon: {
+        color: colors.LightText,
+    }
 });
